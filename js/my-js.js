a99458f0491b41403f410cf57ac8dd93d635c78b
ShaniaZhang <18200390328@163.com>
2017-07-10 15:17:18 +0800

    modify启动页

--- js/my-js.js
+++ js/my-js.js
@@ -3,45 +3,27 @@
  */
 var $j = jQuery.noConflict();
 $j(document).ready(function() {
+    //body高度自适应
     bacCover($j("body"));
-    //首页时间进度条定时器
-    timeChange();
-
-    function timeChange() {
-        var barWth = $j(".time-bar").width();
-        var chgWth = barWth / 1000;
-        var timer = window.setInterval(function() {
-            var wth = $j(".time-bar p").width();
-            if (wth >= barWth) {
-                // $j(".my-body-bac.home-page-two").show();
-                // $j(".my-sy").stop().animate({ 'top': -($j(window).height()) + 'px' }, 40);
-                // setTimeout(function() {
-                //     //css3动画
-                //     $j(".my-pic-bk").removeClass("app-vs1").addClass("app-vs2");
-                //     $j(".my-pc-sp").removeClass("pc-vs1").addClass("pc-vs2");
-                // }, 800);
-                enterHomeTow(40);
-                return;
-            }
-            $j(".time-bar p").css({ "width": wth + chgWth });
-        }, 10);
-    }
+    //启动页时间进度条
+    $j(".time-bar p").addClass('wth100');
+    //启动页时间计数器
     timeNumChg();
-
     function timeNumChg() {
         var timer = window.setInterval(function() {
             var timeNum = parseInt($j(".time-num span").html());
-            if (timeNum >= 10) {
+            if (timeNum == 10) {
+                enterHomeTow(800);
                 return;
             }
             $j(".time-num span").html(timeNum + 1);
         }, 1000);
     }
-    //首页点击next箭头
+    //启动页点击next箭头
     $j(".my-sy-next").click(function() {
         enterHomeTow(800);
     });
-    //首页1回车进入首页2
+    //启动页回车进入首页2
     $j("body").keypress(function(event) {
         if (event && event.keyCode != 13) {
             return;
@@ -49,21 +31,16 @@ $j(document).ready(function() {
         enterHomeTow(800);
     });
     /**
-     * [enterHomeTow description] 进入首页2
+     * [enterHomeTow description] 进入首页
      * @return {[type]} null
      */
     function enterHomeTow(animateTime) {
-        // $j(".my-body-bac.home-page-two").show();
         var ft_height=$j('.my-body-bac.home-page-ft').height();
         $j(".main-con-bg").css('height',ft_height+'px');
         $j(".my-body-bac.home-page-ft").stop().animate({ 'top': -ft_height + 'px' }, animateTime);
         $j(".main-con").css({'margin-top':-ft_height+'px','display':'block'});
         setTimeout(function() {
             $j('.navbar').addClass("sticky-nav");
-            
-        //     //css3动画
-        //     $j(".my-pic-bk").removeClass("app-vs1").addClass("app-vs2");
-        //     $j(".my-pc-sp").removeClass("pc-vs1").addClass("pc-vs2");
         }, animateTime);
     }
     // Sticky Header
